52c51c411c2af1c4ce02592b86a3de02b48b400b
kacai <kacai@microsoft.com>
2021-11-10 15:17:58 +0800

    11/10

--- src/main/java/com/microsoft/azure/webapp/HelloWorldServlet.java
+++ src/main/java/com/microsoft/azure/webapp/HelloWorldServlet.java
@@ -10,12 +10,19 @@ import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.telemetry.RemoteDependencyTelemetry;
+import com.microsoft.applicationinsights.telemetry.Duration;
+import com.microsoft.applicationinsights.telemetry.SeverityLevel;
+
 /**
  * Servlet implementation class HelloWorldServlet
  */
 @WebServlet("/HelloWorldServlet")
 public class HelloWorldServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	static final TelemetryClient telemetryClient = new TelemetryClient();
+	static int visitTimes = 0;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -29,15 +36,38 @@ public class HelloWorldServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        PrintWriter printWriter  = response.getWriter();
+		response.setContentType("text/html");
+		PrintWriter printWriter  = response.getWriter();
+		String info = "01/01";
 
-        //printWriter.println("<h1>Hello World Servlet running on Azure App Service!</h1>");
-		String datetime = "10/25";
-        //SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//Set datetime format
-        //String datetime = df.format(new Date());// new Date() get system current time
-		String info = "<h1>Hello Azure DevOps. Today is " + datetime + "</h1>";
-        printWriter.println(info);
+		// Below for application insight Telemetry tracking demo
+
+		// Track for event
+		telemetryClient.trackEvent("Page Visit");
+		// Track for Metrics
+		telemetryClient.trackMetric("visitTimes", visitTimes++);
+		// Track for dependencies
+		boolean success = false;
+		long startTime = System.currentTimeMillis();
+		try {
+			//printWriter.println("<h1>Hello World Servlet running on Azure App Service!</h1>");
+			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//Set datetime format
+			String datetime = df.format(new Date());// new Date() get system current time
+			//info = "<h1>Hello Azure DevOps. Today is " + datetime + "</h1>";
+		} finally {
+			long endTime = System.currentTimeMillis();
+			RemoteDependencyTelemetry telemetry = new RemoteDependencyTelemetry();
+			telemetry.setSuccess(success);
+			telemetry.setTimestamp(new Date(startTime));
+			telemetry.setDuration(new Duration(endTime - startTime));
+			telemetryClient.trackDependency(telemetry);
+		}
+
+		printWriter.println(info);
+
+		// Track for log
+		telemetryClient.trackTrace("Logs collected by appInsight", SeverityLevel.Warning, null);
+
 	}
 
 	/**
