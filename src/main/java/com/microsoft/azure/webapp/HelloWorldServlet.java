922a449533c1b9773e36ec99121f64927d6211bb
kacai <kacai@microsoft.com>
2021-11-12 14:08:59 +0800

    11/12

--- src/main/java/com/microsoft/azure/webapp/HelloWorldServlet.java
+++ src/main/java/com/microsoft/azure/webapp/HelloWorldServlet.java
@@ -56,10 +56,10 @@ public class HelloWorldServlet extends HttpServlet {
 			// Backend-A.call();
 
 			//SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");//Set datetime format
-			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//Set datetime format
-			String datetime = df.format(new Date());// new Date() get system current time
-
-			info = "<h1>Hello Azure DevOps. Today is " + datetime + "</h1>";
+			
+			// SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//Set datetime format
+			// String datetime = df.format(new Date());// new Date() get system current time
+			// info = "<h1>Hello Azure DevOps. Today is " + datetime + "</h1>";
 		} finally {
 			String dependency = "Backend-A";
 			//long endTime = System.currentTimeMillis();
