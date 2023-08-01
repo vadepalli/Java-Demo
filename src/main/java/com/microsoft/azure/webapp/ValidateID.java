70cee054cbe599b959824f30acb5a9a4a5cf5ce4
Kai Cai <kacai@microsoft.com>
2023-08-01 16:07:18 +0800

    servlet

--- src/main/java/com/microsoft/azure/webapp/ValidateID.java
+++ src/main/java/com/microsoft/azure/webapp/ValidateID.java
@@ -1,1 +1,12 @@
 package com.microsoft.azure.webapp;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+//引入Azure Application Insight SDK库
+import com.microsoft.applicationinsights.TelemetryClient;
+
+//生成ValidateID servlet，访问路径为/ValidateID，实现方法doPost
